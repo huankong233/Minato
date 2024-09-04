3996f3243aa27486d8580cf3bb18d74e1ff7374a
huankong233 <huank0ng@foxmail.com>
2024-09-04 19:45:56 +0800

    [*] 修复 `cron` 时区错误 [+] 增加插件 `callApi`

--- src/libs/cron.ts
+++ src/libs/cron.ts
@@ -1,4 +1,7 @@
 import _cron from 'node-cron'
 
 export const cron = (cron: string, func: () => void) =>
-  _cron.schedule(cron, () => func(), { timezone: process.env.TZ ?? 'Asia/Shanghai' })
+  _cron.schedule(cron, () => func(), {
+    timezone: process.env.TZ ?? 'Asia/Shanghai',
+    scheduled: true
+  })
