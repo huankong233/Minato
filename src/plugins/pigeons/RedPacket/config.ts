6781d6fc8b4e06dc00641ddb75a2ef7ed9847429
huankong233 <huank0ng@foxmail.com>
2024-09-11 19:24:31 +0800

    [*] 移除小写文件夹

--- src/plugins/pigeons/RedPacket/config.ts
+++
@@ -1,7 +0,0 @@
-import type { RedPacket } from '@/global.js'
-import type { Receive } from 'node-napcat-ts'
-
-export type redPackets = RedPacket & {
-  code: (Receive['image'] | Receive['text'] | Receive['face'])[]
-  picked_user: number[]
-}
--- src/plugins/pigeons/mute/config.ts
+++
@@ -1,7 +0,0 @@
-export interface MuteConfig {
-  time: [min: number, max: number]
-}
-
-export const config: MuteConfig = {
-  time: [1, 72000]
-}
