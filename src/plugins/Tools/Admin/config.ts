ff2ab350c4ba05183acf92262bbef5a1f843111a
huankong233 <huank0ng@foxmail.com>
2024-09-11 19:20:23 +0800

    [*] 同步文件大小写

---
+++ src/plugins/Tools/Admin/config.ts
@@ -0,0 +1,35 @@
+export interface AdminConfig {
+  /**
+   * message: 通知
+   * agree: 自动同意
+   */
+  invite: {
+    message: boolean
+    auto: 'accept' | 'reject' | ''
+  }
+  add: {
+    message: boolean
+    auto: 'accept' | 'reject' | ''
+  }
+  friend: {
+    message: boolean
+    auto: 'accept' | 'reject' | ''
+  }
+  notice: boolean
+}
+
+export const config: AdminConfig = {
+  invite: {
+    message: true,
+    auto: 'accept'
+  },
+  add: {
+    message: true,
+    auto: ''
+  },
+  friend: {
+    message: true,
+    auto: 'accept'
+  },
+  notice: true
+}
---
+++ src/plugins/Tools/CheckUpdate/index.ts
@@ -0,0 +1,58 @@
+import { version as local_version } from '@/../package.json'
+import type { allEvents } from '@/global.js'
+import axios from '@/libs/axios.ts'
+import { cron } from '@/libs/cron.ts'
+import { sendMsg } from '@/libs/sendMsg.ts'
+import { BasePlugin } from '@/plugins/Base.ts'
+import { config as botConfig } from '@/plugins/BuiltIn/Bot/config.ts'
+import { compare } from 'compare-versions'
+import { Structs, type AllHandlers } from 'node-napcat-ts'
+import { config } from './config.ts'
+
+export const enable = config.enable
+
+export default class CheckUpdate extends BasePlugin {
+  events: allEvents[] = [
+    {
+      type: 'command',
+      commandName: '检查更新',
+      description: '检查更新',
+      callback: this.checkUpdate.bind(this)
+    }
+  ]
+
+  init = () => cron(config.cron, () => this.checkUpdate())
+
+  async checkUpdate(context?: AllHandlers['message']) {
+    let remote_version = ''
+    try {
+      remote_version = await axios(config.packageJsonUrl).then((res) => res.data.version)
+    } catch (error) {
+      this.logger.ERROR('获取最新版本号失败', error)
+      await sendMsg(context ?? { message_type: 'private', user_id: botConfig.admin_id }, [
+        Structs.text(
+          ['检查更新失败', `当前版本: ${local_version}`, `请检查您的网络状况！`].join('\n')
+        )
+      ])
+      return
+    }
+
+    if (compare(local_version, remote_version, '>=')) {
+      if (!context) return
+      await sendMsg(context, [
+        Structs.text(
+          ['kkbot无需更新哟~', `最新版本: ${remote_version}`, `当前版本: ${local_version}`].join(
+            '\n'
+          )
+        )
+      ])
+    } else {
+      //需要更新，通知admin
+      await sendMsg(context ?? { message_type: 'private', user_id: botConfig.admin_id }, [
+        Structs.text(
+          ['kkbot有更新哟~', `最新版本: ${remote_version}`, `当前版本: ${local_version}`].join('\n')
+        )
+      ])
+    }
+  }
+}
