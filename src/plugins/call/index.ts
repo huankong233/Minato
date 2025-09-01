3d00cf83b9ffce71ee4ec5cf78f81bda84740bbc
huankong233 <huank0ng@foxmail.com>
2025-09-01 14:07:30 +0800

    [*] 跟随框架更新

--- src/plugins/call/index.ts
+++ src/plugins/call/index.ts
@@ -1,4 +1,4 @@
-import { BasePlugin } from '@huan_kong/atri'
+import { BasePlugin, type CommandCallback } from '@huan_kong/atri'
 import { Command } from 'commander'
 import { Structs, type WSSendParam } from 'node-napcat-ts'
 
@@ -8,46 +8,48 @@ export class Plugin extends BasePlugin {
   auto_load_config = false
 
   init() {
-    this.reg_command_event({
+    this.reg_command_event<{ args: [string, string] }>({
       command_name: 'call',
       commander: new Command()
         .description('调用NapCat接口')
         .argument('<action>', '要调用的接口')
         .argument('[params]', '要调用的接口参数', '{}'),
       need_admin: true,
-      callback: async ({ context, args }) => {
-        const [action, params_text] = args
-
-        if (!(action in this.bot.ws)) {
-          this.bot.send_msg(context, [Structs.text(`未找到接口: ${action}`)])
-          return
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        let params: any
-        try {
-          params = JSON.parse(params_text)
-        } catch (_error) {
-          await this.bot.send_msg(context, [Structs.text('请检查传入的参数是否为有效的JSON')])
-          return
-        }
-
-        try {
-          const response = await this.bot.ws[action as keyof WSSendParam](params)
-          const result = await this.bot.send_msg(context, [
-            Structs.text(`接口调用成功: \n${JSON.stringify(response, null, '\t')}`),
-          ])
-          if (!result) {
-            await this.bot.send_msg(context, [
-              Structs.text('接口调用成功, 但是消息发送失败，可能是消息过长或其他原因'),
-            ])
-          }
-        } catch (error) {
-          await this.bot.send_msg(context, [
-            Structs.text(`接口调用失败: \n${JSON.stringify(error, null, '\t')}`),
-          ])
-        }
-      },
+      callback: this.handle_call_command.bind(this),
     })
+  }
+
+  async handle_call_command({ context, args }: CommandCallback<{ args: [string, string] }>) {
+    const [action, params_text] = args
+
+    if (!(action in this.bot.ws)) {
+      this.bot.send_msg(context, [Structs.text(`未找到接口: ${action}`)])
+      return
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let params: any
+    try {
+      params = JSON.parse(params_text)
+    } catch (_error) {
+      await this.bot.send_msg(context, [Structs.text('请检查传入的参数是否为有效的JSON')])
+      return
+    }
+
+    try {
+      const response = await this.bot.ws[action as keyof WSSendParam](params)
+      const result = await this.bot.send_msg(context, [
+        Structs.text(`接口调用成功: \n${JSON.stringify(response, null, '\t')}`),
+      ])
+      if (!result) {
+        await this.bot.send_msg(context, [
+          Structs.text('接口调用成功, 但是消息发送失败，可能是消息过长或其他原因'),
+        ])
+      }
+    } catch (error) {
+      await this.bot.send_msg(context, [
+        Structs.text(`接口调用失败: \n${JSON.stringify(error, null, '\t')}`),
+      ])
+    }
   }
 }
